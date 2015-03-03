1c19ead518c1fca74c4816c093a6cf49db95ea92
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-03 18:40:20 +1100

    Make storage work across both apps and web

--- storage.js
+++ storage.js
@@ -1,4 +1,33 @@
 module.exports = function() {
-  var storage = typeof chrome != 'undefined' ? chrome.storage : null;
-  return storage ? storage.local : localStorage;
+  var chromeStorage = typeof chrome != 'undefined' ? chrome.storage : null;
+  var isApp = !!chromeStorage;
+  var storage = isApp ? chromeStorage.local : localStorage;
+
+  return {
+    get: function(key, callback) {
+      if (isApp) {
+        return storage.get(key, function(values) {
+          callback(null, values[0]);
+        });
+      }
+
+      callback(null, storage.getItem(key));
+    },
+
+    set: function(key, val, callback) {
+      var data;
+      if (isApp) {
+        // create the data object
+        data = {};
+        data[key] = val;
+
+        return storage.set(data, callback);
+      }
+
+      storage.setItem(key, val);
+      if (typeof callback == 'function') {
+        callback(null, storage.getItem(key));
+      };
+    }
+  };
 };
